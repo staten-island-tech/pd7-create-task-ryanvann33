47f92bd909c21f0202276104cba63da75e9210f5
ryanvann33 <rvann377@gmail.com>
2023-03-12 18:02:43 -0400

    wdadawadwdawdaw

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
+const history = [];
+
 function generateMeme() {
   const topText = document.getElementById("topText").value;
   const bottomText = document.getElementById("bottomText").value;
   const imageURL = document.getElementById("imageURL").value;
 
-  const memeContainer = document.getElementById("memeContainer");
-  const memeImage = document.getElementById("memeImage");
-  const topTextContainer = document.getElementById("topTextContainer");
-  const bottomTextContainer = document.getElementById("bottomTextContainer");
-
-  memeImage.src = imageURL;
-  topTextContainer.innerHTML = topText;
-  bottomTextContainer.innerHTML = bottomText;
-
+  const memeContainer = document.createElement("div");
   memeContainer.style.display = "flex";
   memeContainer.style.alignItems = "center";
   memeContainer.style.justifyContent = "center";
   memeContainer.style.position = "relative";
 
+  const memeImage = document.createElement("img");
+  memeImage.src = imageURL;
+  memeContainer.appendChild(memeImage);
+
+  const topTextContainer = document.createElement("div");
+  topTextContainer.innerHTML = topText;
   topTextContainer.style.position = "absolute";
   topTextContainer.style.top = "10%";
   topTextContainer.style.width = "100%";
@@ -25,7 +25,10 @@ function generateMeme() {
   topTextContainer.style.fontWeight = "bold";
   topTextContainer.style.color = "white";
   topTextContainer.style.textShadow = "2px 2px black";
+  memeContainer.appendChild(topTextContainer);
 
+  const bottomTextContainer = document.createElement("div");
+  bottomTextContainer.innerHTML = bottomText;
   bottomTextContainer.style.position = "absolute";
   bottomTextContainer.style.bottom = "10%";
   bottomTextContainer.style.width = "100%";
@@ -34,7 +37,8 @@ function generateMeme() {
   bottomTextContainer.style.fontWeight = "bold";
   bottomTextContainer.style.color = "white";
   bottomTextContainer.style.textShadow = "2px 2px black";
-
-  memeContainer.appendChild(topTextContainer);
   memeContainer.appendChild(bottomTextContainer);
-}
+
+  document.body.appendChild(memeContainer);
+  history.push(memeContainer);
+}  
